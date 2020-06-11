3b1e8289c80c6f056c32b0e98178c1f5da563194
hovnazaryan77 <hovnazaryan77@gmail.com>
2020-06-11 23:27:59 +0400

    small fix

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Quote extends Model
 {
     protected $fillable = ['name', 'email', 'phone', 'zip_code', 'date', 'description', 'reply'];
 
+    public function getCreatedAtAttribute()
+    {
+        return Carbon::createFromFormat("Y-m-d H:i:s", $this->attributes["created_at"])->timezone("America/Los_Angeles")->format("Y-m-d H:i:s");
+    }
 }
